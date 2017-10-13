10989a83e2a28b31a46c7917bf0a3623f123bda7
Linus Ljung <linus.ljung@outlook.com>
2017-10-13 15:30:55 +0200

    Updated apm-list.sh

--- apm-list.sh
+++ apm-list.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 FILE_NAME="installed-atom-packages.txt"
+FILE_PATH="$HOME/dotfiles/$FILE_NAME"
 
 case $1 in
-  "generate") apm list --installed --bare > $FILE_NAME;;
-  "install") apm install --packages-file $FILE_NAME;;
+  "generate") apm list --installed --bare > $FILE_PATH;;
+  "install") apm install --packages-file $FILE_PATH;;
 esac
