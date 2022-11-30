2264182a5269897cad4a3359b06ddd74f96f3106
Linus Ljung <linus.ljung@outlook.com>
2022-11-30 22:06:49 +0100

    Removed atom config

--- apm-list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FILE_NAME="installed-atom-packages.txt"
-FILE_PATH="$HOME/dotfiles/$FILE_NAME"
-
-case $1 in
-  "generate") apm list --installed --bare > $FILE_PATH;;
-  "install") apm install --packages-file $FILE_PATH;;
-esac
